e39e4fb5dc23e157c91ec823c18e640ebf46a606
admin <sameerburli10@gmail.com>
2024-06-09 09:36:31 +0530

    Removing Spring Boot and using only Spring.

--- src/main/java/com/sameer/spring/basics/springDemo/SpringDemoBasicApplication.java
+++ src/main/java/com/sameer/spring/basics/springDemo/SpringDemoBasicApplication.java
@@ -4,13 +4,19 @@ import com.sameer.spring.basics.springDemo.basic.binarySearchImp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration // To Initialize application context
+@ComponentScan("com.sameer.spring.basics.springDemo") // if bean in the same package then the path can be ignored
 public class SpringDemoBasicApplication {
 
 	public static void main(String[] args) {
 		//binarySearchImp bsi = new binarySearchImp(new quickSort()); // Bean
-		ApplicationContext applicationContext = SpringApplication.run(SpringDemoBasicApplication.class, args);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringDemoBasicApplication.class); // To Initialize application context
+				//SpringApplication.run(SpringDemoBasicApplication.class, args);
+
 		binarySearchImp bsi = applicationContext.getBean(binarySearchImp.class);
 		binarySearchImp bsi1 = applicationContext.getBean(binarySearchImp.class);
 		System.out.println(bsi);
